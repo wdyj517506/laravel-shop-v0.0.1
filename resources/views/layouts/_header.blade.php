999c57082f1b8ae1c5c6675323e9292fc5119f0b
vagrant <vagrant@homestead>
2018-06-16 17:14:38 +0000

    用户界面 - 收藏商品列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -28,9 +28,12 @@
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu" role="menu">
-                          <li>
+                            <li>
                             <a href="{{ route('user_addresses.index') }}">收货地址</a>
-                          </li>
+                            </li>
+                            <li>
+                            <a href="{{ route('products.favorites') }}">我的收藏</a>
+                            </li>
                             <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
