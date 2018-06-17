c62061411fd0022dea58f3dbe70ee5b8c13443a4
vagrant <vagrant@homestead>
2018-06-17 20:18:06 +0800

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderPaid;
 use Carbon\Carbon;
 use App\Models\Order;
 use Illuminate\Http\Request;
@@ -57,6 +58,8 @@ class PaymentController extends Controller
             'payment_method' => 'alipay', // 支付方式
             'payment_no'     => $data->trade_no, // 支付宝订单号
         ]);
+
+        $this->afterPaid($order);
 
         return app('alipay')->success();
     }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Listeners\SendOrderPaidMail;
+use App\Events\OrderPaid;
+use App\Listeners\UpdateProductSoldCount;
 use App\Listeners\RegisteredListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
@@ -17,6 +20,13 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             RegisteredListener::class,
+        ],
+        OrderPaid::class => [
+            UpdateProductSoldCount::class,
+        ],
+        OrderPaid::class => [
+            UpdateProductSoldCount::class,
+            SendOrderPaidMail::class,
         ],
     ];
 
